dc8f5aa5916d9ce1bf01d21794fea71f0f6d56df
Hayden Langelier & Oshoke Maiki <pair+haydenlangelier.omaiki@devbootcamp.com>
2016-06-23 16:18:02 -0700

    4 tests left, a few working within

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,24 +1,28 @@
 class GuessingGame
 
-  attr_reader :remaining_guesses
-  attr_accessor :congrats_message
+  attr_accessor :congrats_message, :remaining_guesses
 
 
   def initialize(secret_number,num_of_guesses)
     @secret_number = secret_number
     @num_of_guesses = num_of_guesses
-    @remaining_guesses = remaining_guesses
+    @remaining_guesses = num_of_guesses
     @numbers_guessed=[]
+    @congrats_message = "Yay, you won!"
+    @response = ''
+    @won_games = []
   end
 
-  def congrats_message
-    return "Yay, you won!"
+  # def congrats_message
+  #   return "Yay, you won!"
 
-  end
+  # end
 
   def has_won?
     if @numbers_guessed.include?(@secret_number)
       return true
+    else
+      return false
     end
 
   end
@@ -26,31 +30,39 @@ class GuessingGame
   def has_lost?
     if @remaining_guesses==0
       return true
+    else
+      return false
     end
   end
 
 
   def guess(number)
-    response=''
-    @numbers_guessed<<number
-    if number > @secret_number
-      response<< "Too low!"
-    elsif number < @secret_number
-      response<< "Too high!"
-    else
-      congrats_message + " The number was #{@secret_number}"
-      @numbers_guessed<< @secret_number
-      return true
-    end
 
-    if @numbers_guessed.include?(number)
+    @remaining_guesses -= 1 if !@numbers_guessed.include?(number)
+    @numbers_guessed << number
+    if number < @secret_number
+      @response = "Too low!"
+      @response << " WARNING: Only one guess left!" if remaining_guesses==1
+    elsif number > @secret_number
+      @response = "Too high!"
+      @response << " WARNING: Only one guess left!" if remaining_guesses==1
     else
-      @remaining_guesses -= 1
+      @numbers_guessed << @secret_number
+      @won_games << 1
+      @response = @congrats_message += " The number was #{@secret_number}"
     end
-    if remaining_guesses==1
-      response<<" WARNING: Only one guess left!"
-    end
-    p response
+      if has_won?
+        @response
+      elsif has_lost?
+        return "You lost! The number was #{@secret_number}"
+      end
+
+      # if won_games
+    # elsif has_won?    #@won_games.length > 1
+    #   return "You already won. The number was #{@secret_number}"
+    # else
+      @response
+    # end
   end
 
 end
