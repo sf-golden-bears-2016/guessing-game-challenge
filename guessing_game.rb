055426c610d4926509d125ec3d65766037fc0004
Jasmeet Chatrath & Kenneth Olson <pair+jasmeetchatrath.kennetholson@devbootcamp.com>
2016-06-23 16:09:42 -0700

    unfinished guessing game

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,32 +1,39 @@
 class GuessingGame
   attr_reader :guesses, :guess
-  attr_accessor :congrats_message
+  attr_accessor :congrats_message, :guesses_remaining
 
   def initialize(secret_number, guesses_remaining)
     @secret_number = secret_number
     @guesses_remaining = guesses_remaining
     @congrats_message = "Yay, you won!"
-    @guess = []
+    @guess_array = []
   end
 
-  def guess
-   #@guess << guess
-   #if @guess > @secret_number
-   #  p 'Too high!'
-   #elsif @guess < @secret_number
-   #  p 'Too low!'
-   #else
-   #  congrats_message
-   #end
-   #remaining_guesses
+  def guess(num)
+
+    if num == @secret_number
+      @congrats_message
+    elsif num > @secret_number
+        if @guess_array.include?(num)
+           'Too high!'
+        else
+          @guesses_remaining -= 1
+          @guess_array << num
+           'Too high!'
+        end
+    else num < @secret_number
+        if @guess_array.include?(num)
+           'Too low!'
+        else
+          @guesses_remaining -= 1
+          @guess_array << num
+           'Too low!'
+        end
+    end
   end
 
   def remaining_guesses
-    if guess == false
-      @guesses_remaining -= 1
-    else
-      @guesses_remaining
-    end
+    @guesses_remaining
   end
 
   def has_won?
@@ -42,5 +49,13 @@ class GuessingGame
 end
 
 
-# test = GuessingGame.new(5, 3)
-# p test.remaining_guesses
+ #test = GuessingGame.new(5, 3)
+ #p test.guess(6)
+ #p test.remaining_guesses
+ # p test.guess(2)
+ #p test.remaining_guesses
+ #p test.guess(2)
+ #p test.remaining_guesses
+
+
+
