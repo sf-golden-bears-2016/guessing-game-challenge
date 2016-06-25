f19a7a58bf6c88cedd959d25353683d9f5ad2b08
je-lopez <je.lopez@me.com>
2016-06-25 15:23:31 -0700

    adds methods to check if player has won or lost and begins logic for guessing a number

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,18 +1,43 @@
 class GuessingGame
 	attr_accessor :congrats_message
-	attr_reader :remaining_guesses
+	attr_reader :remaining_guesses, :secret_number, :warning, :numbers_guessed
 
 	def initialize(secret_number, guesses)
 		@secret_number = secret_number
-		@guesses = guesses
-		@congrats_message = "Yay, you won!"
 		@remaining_guesses = guesses
+		@congrats_message = "Yay, you won!"
+		@warning = " WARNING: Only one guess left!"
+		@numbers_guessed = []
 	end
 
 	def has_won?
+		return true if @numbers_guessed.include?(@secret_number)
+		return false if @remaining_guesses >= 1
 	end
 
 	def has_lost?
+		@remaining_guesses <= 0 ? true : false
+	end
+
+	def repeated?(number)
+		@numbers_guessed.include?(number)
+	end
+
+	def guess(number)
+		correct_num = " The number was #{number}"
+
+		"You already won." + correct_num if has_won?
+		"You already lost." + correct_num if has_lost?
+
+		@numbers_guessed << number
+		return @congrats_message + correct_num if number == @secret_number
+
+		@remaining_guesses -= 1 if !repeated?(number)
+		return "You lost!" if has_lost?
+
+		result = number > @secret_number ? "Too high!" : "Too low!"
+		result + @warning if @remaining_guesses == 1 
+		result
 	end
 
 end
