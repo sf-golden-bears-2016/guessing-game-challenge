f6aa08637c757ef1389b9ec9a7fbb04ff5db4674
je-lopez <je.lopez@me.com>
2016-06-25 16:38:59 -0700

    refactors working methods and adds logic to guessing method

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,13 +1,15 @@
 class GuessingGame
-	attr_accessor :congrats_message
-	attr_reader :remaining_guesses, :secret_number, :warning, :numbers_guessed
+	attr_accessor :remaining_guesses, :congrats_message
+	attr_reader :secret_number, :warning, :numbers_guessed, :correct_num
 
 	def initialize(secret_number, guesses)
 		@secret_number = secret_number
 		@remaining_guesses = guesses
+		@numbers_guessed = []
 		@congrats_message = "Yay, you won!"
 		@warning = " WARNING: Only one guess left!"
-		@numbers_guessed = []
+		@correct_num = " The number was "
+		@result = @correct_num + @secret_number.to_s
 	end
 
 	def has_won?
@@ -16,28 +18,32 @@ class GuessingGame
 	end
 
 	def has_lost?
-		@remaining_guesses <= 0 ? true : false
+		@remaining_guesses == 0 ? true : false
 	end
 
 	def repeated?(number)
 		@numbers_guessed.include?(number)
 	end
 
+	def game_over?
+		"You already won." + @result if has_won?
+		"You already lost." + @result if has_lost?
+		false
+	end
+
 	def guess(number)
-		correct_num = " The number was #{number}"
+		game_over?
 
-		"You already won." + correct_num if has_won?
-		"You already lost." + correct_num if has_lost?
-
+		@remaining_guesses -= 1 if !repeated?(number) && !game_over?
 		@numbers_guessed << number
-		return @congrats_message + correct_num if number == @secret_number
 
-		@remaining_guesses -= 1 if !repeated?(number)
-		return "You lost!" if has_lost?
+		return @congrats_message + @result if number == @secret_number
 
-		result = number > @secret_number ? "Too high!" : "Too low!"
-		result + @warning if @remaining_guesses == 1 
-		result
+		return "You lost!" + @result if has_lost?
+
+		return result = number > @secret_number ? "Too high!" : "Too low!"
+		result += @warning if @remaining_guesses == 1
+		return result
 	end
 
 end
